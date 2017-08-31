eec00ae7c7424502e68f9d2b166195a1aec01b74
ciazhar <daisetsunakama@gmail.com>
2017-08-31 23:48:14 +0700

    vault

---
+++ src/main/java/com/ciazhar/configservicevault/configservicevault/ConfigServiceVaultApplication.java
@@ -0,0 +1,12 @@
+package com.ciazhar.configservicevault.configservicevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceVaultApplication.class, args);
+	}
+}
